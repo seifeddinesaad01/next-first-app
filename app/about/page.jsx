b0002b456569b922726b6cb33cc1e3da7a790350
seifeddinesaad01 <saadseif1925@outlook.com>
2023-05-24 11:52:00 +0100

    adding fronts

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+export const metadata ={
+    title: 'about'
+}
 const AboutPage = () => {
   return (
-    <div>saad</div>
+    <div>
+       <h2>This Is Seif Eddine Saad</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eligendi quis sapiente doloribus, dicta eius voluptatem soluta. Odit quae voluptas natus, autem laboriosam nisi? Aut culpa nulla expedita? Nihil repellendus iusto ex laboriosam ipsum pariatur delectus deserunt,
+         cupiditate dolore quas repudiandae modi rerum atque ipsa doloremque aspernatur, adipisci, earum maiores?</p>
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+
 
 export default function Home() {
-  return (
-  <p>Home Page</p>
-  )
+  return <>
+  <h2>SEIF EDDINE SAAD</h2>
+  </>;
 }
